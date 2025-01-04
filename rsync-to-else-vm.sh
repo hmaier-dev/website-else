878fa58227d9cb8bbe8c8cdbb992a3a2c7629fad
Hendrik Maier <hmaier@ipb-halle.de>
2025-01-04 07:39:07 +0100

    feat: remove unneeded script

--- rsync-to-else-vm.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-echo -e "BEWARE:"
-echo -e "First remove else:~/public with root-permissions."
-echo -e "After this you can use this script."
-echo -e ""
-rsync -rav ./public/ else:~/public/
