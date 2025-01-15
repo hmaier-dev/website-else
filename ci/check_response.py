6bca13bfcc001acc2df1dbcef848ceb404a5766d
Mensch <hendrik_maier@protonmail.com>
2025-01-15 12:47:16 +0100

    feat(ci): enhancing the print-message for the user

--- ci/check_response.py
+++ ci/check_response.py
@@ -9,19 +9,21 @@ from typing import Callable
 @dataclass
 class Response:
     uri: str
+    resp_type: str  # just needed for print-statement
     # use lambda here to get the needed requests-Object
     need: Callable
     want: str
 
 
-def check_response(uri, need, want):
+def check_response(uri, resp_type, need, want):
     try:
         response = requests.get(uri)
         actual = need(response)
+        print(f"Testing {uri} for ({resp_type}): ({want}) ")
         if actual == want:
-            print(f"({actual}) matches expected ({want})")
+            print(f"Success: ({actual}) matches expected ({want})")
         else:
-            print(f"Expected {want}, got {actual}")
+            print(f"Failure: Expected ({want}), got ({actual})")
             sys.exit(2)
     except requests.exceptions.RequestException as e:
         print(f"An error occurred while checking {uri}: {e}")
@@ -30,26 +32,31 @@ def check_response(uri, need, want):
 uris = [
     Response(
         uri="https://www.elisa-adam.com/preview",
+        resp_type="Status Code",
         need=lambda r: r.status_code,
         want=200),
     Response(
         uri="https://elisa-adam.com/preview/about-me",
+        resp_type="Status Code",
         need=lambda r: r.status_code,
         want=200),
     Response(
         uri="https://www.elisa-adam.com/preview/workshops/",
+        resp_type="Status Code",
         need=lambda r: r.status_code,
         want=200),
     Response(
         uri="https://www.elisa-adam.com/missing",
+        resp_type="Status Code",
         need=lambda r: r.status_code,
         want=404),
     Response(
         uri="https://elisa-adam.com/preview/media/spass-bei-der-arbeit.webp",
+        resp_type="Content-Type",
         need=lambda r: r.headers.get("Content-Type", ""),
         want="image/webp")
 ]
 
 
 for x in uris:
-    check_response(x.uri, x.need, x.want)
+    check_response(x.uri, x.resp_type, x.need, x.want)
