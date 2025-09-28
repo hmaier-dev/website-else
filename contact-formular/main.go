163579be4bbab69b13490fb7e905f5669b154744
Mensch <hendrik_maier@protonmail.com>
2025-09-28 19:05:13 +0200

    implementing basic auth for api

--- contact-formular/main.go
+++ contact-formular/main.go
@@ -2,19 +2,21 @@ package main
 
 import (
 	"context"
+	"crypto/subtle"
 	"database/sql"
 	_ "embed"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/hmaier-dev/website-else/contact-formular/mailbox"
+	"github.com/jedib0t/go-pretty/v6/table"
 
 	// For future me:
 	// Use 'go get -u github.com/mattn/go-sqlite3' to download it
-	"github.com/hmaier-dev/website-else/contact-formular/mailbox"
-	"github.com/jedib0t/go-pretty/v6/table"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -24,6 +26,12 @@ var ddl string
 type Server struct {
 	Router  *mux.Router
 	Mailbox *mailbox.Queries
+	API 		*API
+}
+
+type API struct {
+	Username string
+	Password string
 }
 
 func main() {
@@ -47,14 +55,31 @@ func NewServer() *Server {
 	if _, err := db.ExecContext(ctx, ddl); err != nil {
 		log.Fatal(err)
 	}
+
+	u := os.Getenv("API_USERNAME")
+	p := os.Getenv("API_PASSWORD")
 	s := &Server{
 		Router:  mux.NewRouter(),
 		Mailbox: mailbox.New(db),
+		API: &API{Username: u, Password: p,},
 	}
-	s.Router.HandleFunc("/contact", s.HandlerContactRequest).Methods("POST")
 	mailbox := s.Router.PathPrefix("/mailbox").Subrouter()
-	mailbox.HandleFunc("/all", s.HandlerAllMessages).Methods("GET")
-	mailbox.HandleFunc("/unread", s.HandlerUnreadMessages).Methods("GET")
+	mailbox.HandleFunc("/contact", s.HandlerContactRequest).Methods("POST")
+	if u != "" && p != ""{
+		mailbox.HandleFunc("/all", s.HandlerAllMessages).Methods("GET")
+		mailbox.HandleFunc("/unread", s.HandlerUnreadMessages).Methods("GET")
+	}else{
+		log.Println("Didn't start the api because username/password weren't set.")
+	}
+
+
+	// logs all routes when starting after they go defined
+	s.Router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
+		path, _ := route.GetPathTemplate()
+		method, _ := route.GetMethods()
+		log.Println(method, path)
+		return nil
+	})
 
 	return s
 }
@@ -69,7 +94,7 @@ func (s *Server) HandlerContactRequest(w http.ResponseWriter, r *http.Request) {
 	log.Println(name, email, message, approval)
 	// TODO: Sanatizing and validating the input
 	ctx := context.Background()
-	if approval != "false" {
+	if approval == "on" {
 		err := s.Mailbox.AddMessage(ctx, mailbox.AddMessageParams{
 			Name:     name,
 			Email:    email,
@@ -80,17 +105,28 @@ func (s *Server) HandlerContactRequest(w http.ResponseWriter, r *http.Request) {
 		})
 		if err != nil {
 			http.Error(w, "Something wen't wrong storing the data...", http.StatusInternalServerError)
+			log.Fatalf("Err: %v", err)
 		}
 		w.WriteHeader(http.StatusOK)
+		return
 	}
 	http.Error(w, "We need approval to store user data", http.StatusForbidden)
 }
 
 // Serving GET request to return all messages
 func (s *Server) HandlerAllMessages(w http.ResponseWriter, r *http.Request) {
-	 
-	// TODO: Implement basic auth
-	// u, p, ok := r.BasicAuth()
+
+	// Refactor this into one function
+	// Here is how to do it
+	// https://stackoverflow.com/questions/21936332/idiomatic-way-of-requiring-http-basic-auth-in-go
+	u, p, ok := r.BasicAuth()
+	realm := "Einmal bitte Credentials eingeben: "
+	if !ok || subtle.ConstantTimeCompare([]byte(u), []byte(s.API.Username)) != 1 || subtle.ConstantTimeCompare([]byte(p), []byte(s.API.Password)) != 1 {
+		w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+		w.WriteHeader(401)
+		w.Write([]byte("Unauthorised.\n"))
+		return
+	}
 
 	ctx := context.Background()
 	all, err := s.Mailbox.GetAllMessages(ctx)
@@ -99,27 +135,41 @@ func (s *Server) HandlerAllMessages(w http.ResponseWriter, r *http.Request) {
 	}
 	t := table.NewWriter()
 	t.SetOutputMirror(w)
-	t.AppendHeader(table.Row{"ID","Name","Email","Message","Date","Approval","IsRead"})
+	t.AppendHeader(table.Row{"ID", "Name", "Email", "Message", "Date", "Approval", "IsRead"})
 	for _, a := range all {
 		d := time.Unix(a.Date, 0).Format("02.01.2006 15:04:05")
-		t.AppendRow(table.Row{a.ID,a.Name,a.Email,a.Message,d,a.Approval.Int64,a.Isread.Int64})
+		t.AppendRow(table.Row{a.ID, a.Name, a.Email, a.Message, d, a.Approval.Int64, a.Isread.Int64})
 	}
 	t.SetStyle(table.StyleLight)
 	t.Render()
 }
+
+
 func (s *Server) HandlerUnreadMessages(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	unread, err := s.Mailbox.GetUnreadMessage(ctx)
+
+	// Refactor this into one function
+	// Here is how to do it
+	// https://stackoverflow.com/questions/21936332/idiomatic-way-of-requiring-http-basic-auth-in-go
+	u, p, ok := r.BasicAuth()
+	realm := "Einmal bitte Credentials eingeben: "
+	if !ok || subtle.ConstantTimeCompare([]byte(u), []byte(s.API.Username)) != 1 || subtle.ConstantTimeCompare([]byte(p), []byte(s.API.Password)) != 1 {
+		w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+		w.WriteHeader(401)
+		w.Write([]byte("Unauthorised.\n"))
+		return
+	}
 
 	if err != nil {
 		http.Error(w, "Something wen't wrong querying unread messages...", http.StatusInternalServerError)
 	}
 	t := table.NewWriter()
 	t.SetOutputMirror(w)
-	t.AppendHeader(table.Row{"ID","Name","Email","Message","Date","Approval","IsRead"})
+	t.AppendHeader(table.Row{"ID", "Name", "Email", "Message", "Date", "Approval", "IsRead"})
 	for _, u := range unread {
 		d := time.Unix(u.Date, 0).Format("02.01.2006 15:04:05")
-		t.AppendRow(table.Row{u.ID,u.Name,u.Email,u.Message,d,u.Approval.Int64,u.Isread.Int64})
+		t.AppendRow(table.Row{u.ID, u.Name, u.Email, u.Message, d, u.Approval.Int64, u.Isread.Int64})
 	}
 	t.SetStyle(table.StyleLight)
 	t.Render()
